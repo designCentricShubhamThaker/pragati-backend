9f8f0187b129f76f587e0d76dc020a74059f1365
designCentricShubhamThaker <shubham@designcentric.co.in>
2025-05-08 15:09:55 +0530

    renderrr

--- server.mjs
+++ server.mjs
@@ -10,10 +10,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true
-}));
+app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
