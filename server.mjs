8368b100a5b5cd1dd491f2115c37b8569d146207
designCentricShubhamThaker <shubham@designcentric.co.in>
2025-05-08 15:39:01 +0530

    prod url added

--- server.mjs
+++ server.mjs
@@ -10,7 +10,10 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "https://pragati-glass-frontend.vercel.app",
+  credentials: true
+}));
 
 const PORT = process.env.PORT || 5000;
 
